86ee64dddc101fdd8c613163cf2447472a278010
EmericSauthier <Emeric.Sauthier@etu.univ-smb.fr>
2024-02-05 08:28:07 +0100

    Classe partielle Serie

--- WSSeries/Models/EntityFramework/Serie.cs
+++ WSSeries/Models/EntityFramework/Serie.cs
@@ -32,38 +32,4 @@ public partial class Serie
     [Column("network")]
     [StringLength(50)]
     public string? Network { get; set; }
-
-    public Serie() { }
-    public Serie(int serieid, string titre)
-    {
-        this.Serieid = serieid;
-        this.Titre = titre;
-    }
-
-    public Serie(int serieid, string titre, string? resume, int? nbsaisons, int? nbepisodes, int? anneecreation, string? network) : this(serieid,titre)
-    {
-        this.Resume = resume;
-        this.Nbsaisons = nbsaisons;
-        this.Nbepisodes = nbepisodes;
-        this.Anneecreation = anneecreation;
-        this.Network = network;
-    }
-
-
-    public override bool Equals(object? obj)
-    {
-        return obj is Serie serie &&
-               this.Serieid == serie.Serieid &&
-               this.Titre == serie.Titre &&
-               this.Resume == serie.Resume &&
-               this.Nbsaisons == serie.Nbsaisons &&
-               this.Nbepisodes == serie.Nbepisodes &&
-               this.Anneecreation == serie.Anneecreation &&
-               this.Network == serie.Network;
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(this.Serieid, this.Titre, this.Resume, this.Nbsaisons, this.Nbepisodes, this.Anneecreation, this.Network);
-    }
 }
---
+++ WSSeries/Models/SeriePart.cs
@@ -0,0 +1,39 @@
+﻿namespace WSSeries.Models.EntityFramework
+{
+    public partial class Serie
+    {
+        public Serie() { }
+        public Serie(int serieid, string titre)
+        {
+            this.Serieid = serieid;
+            this.Titre = titre;
+        }
+
+        public Serie(int serieid, string titre, string? resume, int? nbsaisons, int? nbepisodes, int? anneecreation, string? network) : this(serieid, titre)
+        {
+            this.Resume = resume;
+            this.Nbsaisons = nbsaisons;
+            this.Nbepisodes = nbepisodes;
+            this.Anneecreation = anneecreation;
+            this.Network = network;
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Serie serie &&
+                   this.Serieid == serie.Serieid &&
+                   this.Titre == serie.Titre &&
+                   this.Resume == serie.Resume &&
+                   this.Nbsaisons == serie.Nbsaisons &&
+                   this.Nbepisodes == serie.Nbepisodes &&
+                   this.Anneecreation == serie.Anneecreation &&
+                   this.Network == serie.Network;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Serieid, this.Titre, this.Resume, this.Nbsaisons, this.Nbepisodes, this.Anneecreation, this.Network);
+        }
+    }
+}
